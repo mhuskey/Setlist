4ec3392e620517ca450ea2c7062cc9843f2282fb
mhuskey <matthewhuskey@me.com>
2019-10-28 11:44:18 -0400

    Export, Import, Print & Performance View
    • Added ability to export the songs which creates a modal with the URL
    • Added ability to import songs via the formatted URL
    • Added a PDF print view
    • Added a performance view in a modal with the song list that is read-only
    • Made minor changes to the UI, such as <hr> separators between groups of buttons

--- assets/javascripts/setlist.js
+++ assets/javascripts/setlist.js
@@ -1,9 +1,11 @@
 $("document").ready(function() {
+  importSongs();
+  
   // Clear text box when page loads
   $("#song-name").val("");
   
   // Reorder list elements by dragging and dropping
-  $("ol").sortable().draggable({
+  $(".songs").sortable().draggable({
     // Return the 'ol' to their starting position
     revert: true
   });
@@ -17,7 +19,7 @@ $("document").ready(function() {
       ui.draggable.remove();
     }
   });
-  $("ol").disableSelection();
+  $(".songs").disableSelection();
   
   // Add song to list after clicking button
   $("#song-button").click(function() {
@@ -47,33 +49,12 @@ $("document").ready(function() {
   
   // Add 'Break' header to list
   $("#break-button").click(function() {
-    $("ol").append("<h3 class='break text-center'>* Break *</h3>");
+    $("ol").append("<h3 class='break text-center'>*Break*</h3>");
   });
   
   // Add 'Encore' header to list
   $("#encore-button").click(function() {
-    $("ol").append("<h3 class='encore text-center'>* Encore *</h3>");
-  });
-  
-  // Clear text box when form gains focus
-  $("#song-name").focus(function() {
-    $(this).val("");
-  });
-  
-  // Fade out song, and then call `remove()` as callback
-  // function, which removes element from the DOM
-  $(document).on("dblclick", "li", function() {
-    $(this).fadeOut(function() {
-      $(this).remove();
-    });
-  });
-  
-  // Fade out 'Break' or 'Encore', and then call `remove()`
-  // as callback function, which removes element from the DOM
-  $(document).on("dblclick", "h3", function() {
-    $(this).fadeOut(function() {
-      $(this).remove();
-    });
+    $("ol").append("<h3 class='encore text-center'>*Encore*</h3>");
   });
   
   // Print list of songs
@@ -88,7 +69,129 @@ $("document").ready(function() {
     printWindow.document.write('</body></html>');
     printWindow.document.close();
   });
+  
+  // Open export modal with URL to export song list
+  $("#export-button").click(function() {
+    // Regex for removing any songs in the URL
+    var urlRegex = /\?.*/g;
+    // Get the base URL and begin formatting for export
+    var exportURL = window.location.href;
+    // Reset the URL to base URL without any songs
+    exportURL = exportURL.replace(urlRegex, "");
+    // Get the number of songs, if any
+    var lengthOfSongs = $(".songs > *").length;
+    // Build up URL if songs exist
+    if (lengthOfSongs !== 0) {
+      exportURL += "?";
+      for (var i = 0; i < lengthOfSongs; i++) {
+        var originalSongName = $($("ol > *")[i]).text();
+        // Replace all spaces with plus symbols
+        originalSongName = originalSongName.replace(/\s{1,}/g,'+');
+        // Add each song to the end of the URL with a comma
+        exportURL += originalSongName + ",";
+      }
+      // Remove last comma
+      exportURL = exportURL.slice(0, -1);
+      // Add closing backslash
+      exportURL += "/";
+    }
+    // Populate modal with URL
+    $("#export-url").text(exportURL);
+    // Show modal to user
+    $('#exportSongsModal').modal();
+  });
+  
+  // Copy link to user's clipboard
+  $("#copy-button").click(function() {
+    // Get export URL
+    var linkToCopy = $("#export-url").text();
+    // Cannot select text from read-only element,
+    // so <textarea> is created with the export URL
+    var hiddenInput = $("<textarea>" + linkToCopy + "</textarea>");
+    // Add the <textarea> to the modal
+    $("#exportSongsModalBody").append(hiddenInput);
+    // Select the export URL
+    hiddenInput.select();
+    // Copy the export URL to the user's clipboard
+    document.execCommand("copy");
+    // Remove <textarea> from the DOM
+    hiddenInput.remove();
+  });
+  
+  // Enable Bootstrap's popover
+  $(function () {
+    $('[data-toggle="popover"]').popover();
+  });
+  
+  // Open performance modal
+  $("#performance-button").click(function() {
+    // Reset performance song list
+    $("#performanceSongList").empty();
+    // Get list of songs from .songs
+    var songList = $(".songs").html();
+    // Add songs to performance song list
+    $("#performanceSongList").append(songList);
+  });
+  
+  // Clear text box when form gains focus
+  $("#song-name").focus(function() {
+    $(this).val("");
+  });
+  
+  // Fade out song in .songs, and then call `remove()` as callback
+  // function, which removes element from the DOM
+  $(document).on("dblclick", ".songs li", function() {
+    $(this).fadeOut(function() {
+      $(this).remove();
+    });
+  });
+  
+  // Fade out 'Break' or 'Encore' in .songs, and then call `remove()`
+  // as callback function, which removes element from the DOM
+  $(document).on("dblclick", ".songs h3", function() {
+    $(this).fadeOut(function() {
+      $(this).remove();
+    });
+  });
 });
+
+// Receive URL and import songs
+function importSongs() {
+  // Get URL and parse for songs between braces
+  var url = window.location.href;
+  // Regex to return songs from url
+  var urlRegex = /html\?(.*?)\//;
+  
+  if (urlRegex.exec(url) !== null) {
+    var urlString = urlRegex.exec(url)[1];
+  } else {
+    var urlString = null;
+  }
+  
+  // If songs to import exist
+  if (urlString !== null) {
+    // Convert "+" to spaces
+    urlString = urlString.replace(/\+/g," ");
+    // Convert "%20" to spaces
+    urlString = urlString.replace(/%20/g," ");
+    // Convert "%27" to apostrophe
+    urlString = urlString.replace(/%27/g,"'");
+    // Convert "%22" to quotation marks
+    urlString = urlString.replace(/%22/g,"\"");
+    // Convert formatted songs to an array and remove commas
+    var urlSongs = urlString.split(",");
+    for (var i = 0; i < urlSongs.length; i++) {
+      // Loop over array and append each item to the list of songs
+      if (urlSongs[i] === "*Break*") {
+        $("ol").append("<h3 class='break text-center'>" + urlSongs[i] + "</h3>");
+      } else if (urlSongs[i] === "*Encore*") {
+        $("ol").append("<h3 class='encore text-center'>" + urlSongs[i] + "</h3>");
+      } else {
+        $("ol").append("<li class='text-center'>" + urlSongs[i] + "</li>");
+      }
+    }
+  }
+}
 
 // Add song function
 function addSong() {
